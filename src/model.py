e91c4be0eac15d631579c816980824d24978d653
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-08-29 16:15:52 +0200

    current setup
    Signed-off-by: Kent Madsen <kent.vejrup.madsen@designermadsen.dk>

--- src/model.py
+++ src/model.py
@@ -1,21 +1,29 @@
 from keras_cv.models    \
     import              \
-        YOLOV8Detector, \
-        YOLOV8Backbone
+    YOLOV8Detector,     \
+    YOLOV8Backbone
 
-from labels                 \
-    import                  \
-        get_configuration,  \
-        get_size_of_labels
+from labels             \
+    import              \
+    get_size_of_labels
 
+def get_model():
+    global model 
 
-detector = YOLOV8Detector(
-    num_classes = get_size_of_labels(),
-    bounding_box_format = 'xyxy',
-    
-    backbone=YOLOV8Backbone.from_preset(
-        'yolo_v8_l_backbone_coco'
-    ),
+    if model is None:
+        setup_model()
 
-    fpn_depth=2
-)
+    return model
+
+def setup_model() -> None:
+    global model
+    model = YOLOV8Detector(
+        num_classes = get_size_of_labels(),
+        bounding_box_format = 'xyxy',
+        
+        backbone=YOLOV8Backbone.from_preset(
+            'yolo_v8_l_backbone_coco'
+        ),
+
+        fpn_depth=2
+    )
