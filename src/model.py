1ea72478d1e78e4bf9f119ecb9170cec959dc079
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-08-29 21:34:13 +0200

    c

--- src/model.py
+++ src/model.py
@@ -3,23 +3,3 @@ from keras_cv.models    \
     YOLOV8Detector,     \
     YOLOV8Backbone
 
-def get_model():
-    global model 
-
-    return model
-
-def setup_model(
-    size_of_categories: int
-) -> None:
-    global model
-
-    model = YOLOV8Detector(
-        num_classes = size_of_categories,
-        bounding_box_format = 'xyxy',
-        
-        backbone=YOLOV8Backbone.from_preset(
-            'yolo_v8_l_backbone_coco'
-        ),
-
-        fpn_depth=4
-    )
